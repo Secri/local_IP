4fd05ebdda77efce6183f4d3bc24867f74e6d068
Secri <44509048+Secri@users.noreply.github.com>
2025-07-04 11:23:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,7 @@ import sys
 import threading
 import time
 import socket
-from tkinter import *
-from PIL import Image, ImageTk
-import pystray
-from pystray import Icon as Icon, MenuItem as MenuItem
-import darkdetect
+from tkinter import Tk, Label, Button, PhotoImage
 
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
@@ -17,7 +13,6 @@ def resource_path(relative_path):
 
 IMG_DIR = resource_path("assets/images")
 OCTOPUS = f"{IMG_DIR}/octopus.png"
-OCTOPUS_THUMB = f"{IMG_DIR}/octopus_thumb.png"
 CLOSE = f"{IMG_DIR}/close.png"
 
 class Application:
@@ -32,65 +27,36 @@ class Application:
         self.root.title("MyIP Widget")
         self.root.iconbitmap(f"{IMG_DIR}\\icon.ico")
 
-        self.detect_theme()
-
         # Définir une police personnalisée avec Tahoma
         self.custom_font = ("Tahoma", 14, "bold")
 
-        self.lab1 = Label(self.root, bg=self.bg_color, padx=5)
-        self.lab2 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0, font=self.custom_font)
-        self.lab3 = Label(self.root, bd=0, bg=self.bg_color, fg=self.fg_color, highlightthickness=0, borderwidth=0, font=self.custom_font)
+        self.lab1 = Label(self.root, padx=5)
+        self.lab2 = Label(self.root, bd=0, highlightthickness=0, borderwidth=0, font=self.custom_font)
+        self.lab3 = Label(self.root, bd=0, highlightthickness=0, borderwidth=0, font=self.custom_font)
 
         self.lab1.grid(row=1, column=1, padx=5)
         self.lab2.grid(row=2, column=1, padx=5)
         self.lab3.grid(row=3, column=1, padx=5, pady=(0, 5))
 
         # Créer un bouton en forme de "X" pour masquer la fenêtre
-        self.close_image = ImageTk.PhotoImage(file=CLOSE)
-        self.close_button = Button(self.root, image=self.close_image, cursor="hand1", bg=self.bg_color, fg=self.fg_color, bd=0, highlightthickness=0)
+        self.close_image = PhotoImage(file=CLOSE)
+        self.close_button = Button(self.root, image=self.close_image, cursor="hand1", bd=0, highlightthickness=0)
         self.close_button.bind("<Button-1>", lambda event: self.quit_window())
         self.close_button.grid(row=0, column=1, padx=(0, 5), pady=(5, 0), sticky="ne")
 
-        self.icon = pystray.Icon("ping")
-        self.icon.icon = Image.open(OCTOPUS_THUMB)
-        self.icon.run_detached()
-        self.icon.menu = (
-            MenuItem('Quit', lambda: self.quit_window()),
-            MenuItem('Show', self.show_window)
-        )
         self.root.attributes("-alpha", 0.85)
         self.root.attributes('-topmost', True)
-        self.root.configure(bg=self.bg_color)
         self.root.bind("<B1-Motion>", self.move_window)
 
         self.thread2 = threading.Thread(target=self.update_data)
         self.thread2.start()
-
-    def detect_theme(self):
-        theme = darkdetect.theme()
-
-        if theme == "Dark":
-            self.bg_color = "black"
-            self.fg_color = "white"
-        else:
-            self.bg_color = "white"
-            self.fg_color = "black"
 
     def move_window(self, event):
         self.root.geometry(f'+{event.x_root}+{event.y_root}')
 
     def quit_window(self):
         self.stop_program = True
-        self.icon.icon = None
-        self.icon.title = None
-        self.icon.stop()
         self.root.destroy()
-
-    def show_window(self):
-        self.root.after(0, self.root.deiconify())
-
-    def hide_window(self, event):
-        self.root.withdraw()
 
     def find_ip(self):
         try:
@@ -108,19 +74,16 @@ class Application:
                 ip_address = ip["query"]
                 if ip_address != self.last_ip:
                     self.last_ip = ip_address
-                    self.lab1.image = ImageTk.PhotoImage(image=Image.open(OCTOPUS))
+                    self.lab1.image = PhotoImage(file=OCTOPUS)
                     self.lab1.config(image=self.lab1.image)
                     self.lab2.config(text=ip["country"])
                     self.lab3.config(text=ip_address)
-                    self.icon.icon = Image.open(OCTOPUS_THUMB)
-                    self.icon.title = ip_address
             else:
                 self.last_ip = None
-                self.lab1.image = ImageTk.PhotoImage(file=OCTOPUS)
+                self.lab1.image = PhotoImage(file=OCTOPUS)
                 self.lab1.config(image=self.lab1.image)
                 self.lab2.config(text="No Internet")
                 self.lab3.config(text="")
-                self.icon.icon = Image.open(OCTOPUS_THUMB)
             time.sleep(60)
 
     def run(self):
